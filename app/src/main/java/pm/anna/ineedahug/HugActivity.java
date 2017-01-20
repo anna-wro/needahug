18269be295a7065ff159a8d70f4296206873fcca
Anna <wro.emails@gmail.com>
2017-01-20 18:29:36 +0100

    Alternative Resources

--- app/src/main/java/pm/anna/ineedahug/HugActivity.java
+++ app/src/main/java/pm/anna/ineedahug/HugActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 
@@ -12,6 +13,7 @@ public class HugActivity extends AppCompatActivity {
     private TextView mHugTextView;
     private Button mHugButton;
     private RelativeLayout mRelativeLayout;
+    private ScrollView mScrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         final ColorWheel mColorWheel = new ColorWheel(this);
@@ -23,14 +25,17 @@ public class HugActivity extends AppCompatActivity {
         mHugTextView = (TextView) findViewById(R.id.hugTextView);
         mHugButton = (Button) findViewById(R.id.showHugButton);
         mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_hug);
+        mScrollView = (ScrollView) findViewById(R.id.scroll);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String hugs = mAllHugs.getHug();
                 int color = mColorWheel.getColor();
                 mHugTextView.setText(hugs);
+                mScrollView.setBackgroundColor(color);
                 mRelativeLayout.setBackgroundColor(color);
                 mHugButton.setTextColor(color);
+
             }
         };
         mHugButton.setOnClickListener(listener);
