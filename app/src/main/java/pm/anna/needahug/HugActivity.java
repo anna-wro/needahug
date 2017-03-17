230b699d4f874e2feca368e26dd936591aa2fb2d
anna-wro <wro.online@gmail.com>
2017-03-17 12:31:47 +0100

    :wrench: Boom menu button

--- app/src/main/java/pm/anna/needahug/HugActivity.java
+++ app/src/main/java/pm/anna/needahug/HugActivity.java
@@ -1,18 +1,20 @@
 package pm.anna.needahug;
 
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.Util;
 
 public class HugActivity extends BaseActivity {
     private TextView mHugTextView;
@@ -47,6 +49,18 @@ public class HugActivity extends BaseActivity {
         mHugButton = (Button) findViewById(R.id.showHugButton);
         mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_hug);
         mScrollView = (ScrollView) findViewById(R.id.scroll);
+        BoomMenuButton bmb = (BoomMenuButton) findViewById(R.id.bmb);
+        for (int i = 0; i < bmb.getButtonPlaceEnum().buttonNumber(); i++) {
+            bmb.addBuilder(new SimpleCircleButton.Builder()
+                    .normalImageRes(R.drawable.ic_share_white_24dp)
+                    .imageRect(new Rect(8, 12, 42,42))
+                    .shadowEffect(false)
+                    .rippleEffect(true)
+                    .normalColorRes(R.color.colorPrimary)
+                    .buttonRadius(Util.dp2px(18))
+                    .pieceColorRes(R.color.semi_transparent));
+        }
+
         mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         mToolbar.setTitle("");
         setSupportActionBar(mToolbar);
@@ -69,28 +83,25 @@ public class HugActivity extends BaseActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
-        MenuItem item = menu.findItem(R.id.shareButton);
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
-        this.getWindow().setBackgroundDrawable(new ColorDrawable(255));
         return true;
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.shareButton:
-                String shareHug = mHugTextView.getText().toString();
-                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                sharingIntent.setType("text/plain");
-                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareHug);
-                startActivity(Intent.createChooser(sharingIntent,  getResources().getString(R.string.share)));
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        switch (item.getItemId()) {
+//            case R.id.shareButton:
+//                String shareHug = mHugTextView.getText().toString();
+//                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+//                sharingIntent.setType("text/plain");
+//                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareHug);
+//                startActivity(Intent.createChooser(sharingIntent,  getResources().getString(R.string.share)));
+//                return true;
+//
+//            default:
+//                // If we got here, the user's action was not recognized.
+//                // Invoke the superclass to handle it.
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//    }
 }
