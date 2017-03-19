dfe1701890ac27dc58447f4a9542873577ee88a2
anna-wro <wro.online@gmail.com>
2017-03-19 13:30:11 +0100

    :sparkles: Get hugs from the database

--- app/src/main/java/pm/anna/needahug/AllHugs.java
+++ app/src/main/java/pm/anna/needahug/AllHugs.java
@@ -1,21 +1,31 @@
 package pm.anna.needahug;
+
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.database.Cursor;
 
 import java.util.Random;
 
+import pm.anna.needahug.data.HugsContract;
+
 public class AllHugs extends ContextWrapper  {
 
-    public String[] mHugs;
     public AllHugs (Context mContext) {
         super(mContext);
-        mHugs = getResources().getStringArray(R.array.hugs);
     }
 
     public String getHug(){
-        // Randomly select a text
         Random randomGenerator = new Random();
-        int randomNumber = randomGenerator.nextInt(mHugs.length);
-        return mHugs[randomNumber];
+        // Get hugs from the database
+        Cursor c = getContentResolver().query(HugsContract.HugsEntry.CONTENT_URI, null, null, null, null);
+        assert c != null;
+        // Check how many hugs are there
+        int hugsAvailable = c.getCount();
+        // Randomly select a hug
+        int randomNumber = randomGenerator.nextInt(hugsAvailable);
+        c.moveToPosition(randomNumber);
+        // Get hug!
+        String hug = c.getString(c.getColumnIndex("hug"));
+        return hug;
     }
 }
